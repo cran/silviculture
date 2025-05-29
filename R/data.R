3615ebf4bf42c2c74c04ae1e14e712d5866c4295
Adrián Cidre González <adrian.cidre@gmail.com>
2025-05-29 08:50:02 +0000

    version 0.1.0

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Forest inventory samples
+#'
+#' Inventory data from Spanish National Forest Inventory
+#'
+#' @format A `tibble`
+"inventory_samples"
+
+
+#' Biomass models
+#'
+#' Biomass models available in \code{silviculture} package. If you would like
+#' to suggest new models, please open a new issue.
+#'
+#' @format A `tibble`
+"biomass_models"
---
+++ R/globals.R
@@ -0,0 +1,19 @@
+utils::globalVariables(
+  c(
+    "biomass_models",
+    "d",
+    "dclass",
+    "dg",
+    "g_ha",
+    "h",
+    "h0",
+    "n",
+    "nt",
+    "ntrees",
+    "ntrees_ha",
+    "weighted.mean",
+    ".cumtrees",
+    ".do",
+    ".nmax"
+  )
+)
